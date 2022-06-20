5dfd6270a709db8a91d05d3721d021f012b2a46c
R0dk0 <rodko.dimitrov@gmail.com>
2022-06-20 14:54:20 +0300

    Update

---
+++ src/main/java/Formula1/example/Formula1/model/Driver.java
@@ -0,0 +1,108 @@
+package Formula1.example.Formula1.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.awt.*;
+import java.util.Calendar;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "drivers")
+
+public class Driver {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String surname;
+
+    @Column(nullable = false)
+    private String lastname;
+
+
+    @Column(nullable = false)
+    private Image driverImage;
+
+    @Lob
+    @Column(nullable = false)
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "born_id", nullable = false)
+    private Calendar born;
+
+    @ManyToOne
+    @JoinColumn (name = "nationality_id", nullable = false)
+    private Nationality nationality;
+
+    @Column(nullable = false)
+    private String status;
+
+    @Column(nullable = false)
+    private String seasonActivity;
+
+    @ManyToOne
+    @JoinColumn(name = "carNumber_id", nullable = false)
+    private CarNumber carNumber;
+
+    @Column(nullable = false)
+    private String currentTeam;
+
+    @Column(nullable = false)
+    private String previousTeams;
+
+    @ManyToOne
+    @JoinColumn(name = "wordTitles_id", nullable = false)
+    private WorldTitle worldTitles;
+
+    @Column(nullable = false)
+    private String races;
+
+    @ManyToOne
+    @JoinColumn(name = "poles_id", nullable = false)
+    private PolePosition poles;
+
+    @ManyToOne
+    @JoinColumn(name = "wins_id", nullable = false)
+    private Win wins;
+
+    @ManyToOne
+    @JoinColumn(name = "podiums_id", nullable = false)
+    private Podium podiums;
+
+    @ManyToOne
+    @JoinColumn(name = "fastestLaps_id", nullable = false)
+    private FastestLap fastestLaps;
+
+    @ManyToOne
+    @JoinColumn(name = "points", nullable = false)
+    private Point points;
+
+    @Column(nullable = false)
+    private String firstRace;
+
+    @Column(nullable = false)
+    private String latestRace;
+
+    @Column(nullable = false)
+    private String firstWin;
+
+    @Column(nullable = false)
+    private String latestWin;
+
+
+
+}
